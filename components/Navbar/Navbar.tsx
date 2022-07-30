ef2ebf53f765edbb7c0cdc2019677412059ba0f2
Angelo Longoriah <arlongoria93@gmail.com>
2022-07-30 12:25:50 -0500

    navbar changed upon mobile -> desktop

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,19 +3,26 @@ import NavbarItem from "../NavbarItem/NavbarItem";
 
 const Navbar = () => {
   return (
-    <div className="p-4">
-      <ul className="navbar  bg-accent p-2 rounded-box w-full h-full">
-        <div className="navbar-start">
+    <div className="p-4 h-screen overflow-hidden">
+      <ul className="md:h-full content-center navbar md:flex md:flex-col md:space-between items-center justify-between bg-accent p-2 rounded-box ">
+        <div className="navbar-start md:flex md:flex-col space-y-8">
           <NavbarItem icon="movie" />
+          <div className="navbar-center hidden md:inline-flex menu menu-horizontal md:menu-vertical">
+            <NavbarItem icon="shape" />
+            <NavbarItem icon="film" />
+            <NavbarItem icon="tv" />
+            <NavbarItem icon="bookmark" />
+          </div>
         </div>
-        <div className="navbar-center menu menu-horizontal">
+
+
+        <div className="md:hidden navbar-center menu menu-horizontal md:menu-vertical">
           <NavbarItem icon="shape" />
           <NavbarItem icon="film" />
           <NavbarItem icon="tv" />
           <NavbarItem icon="bookmark" />
         </div>
-
-        <div className="navbar-end">
+        <div className="navbar-end md:flex md:flex-col ">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
               <img src="https://placeimg.com/80/80/people" />
