b8bdc0bc7c75ed85703159cc26639c1c217cfc30
Angelo Longoriah <arlongoria93@gmail.com>
2022-08-01 11:55:32 -0500

    fetching data and working on trending section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
 import type { NextPage } from "next";
 import Navbar from "../components/Navbar/Navbar";
+import useQuery from '@tanstack/react-query'
+import data from '../data.json'
+import Image from "next/image";
+import { useEffect, useState } from 'react'
 
-const Home: NextPage = () => {
+interface Movies {
+  title: string
+}
+
+const Home: NextPage = (Movies) => {
+  const [movies, setMovies] = useState(data)
+  console.log(movies)
   return (
     <div className="font-sans flex flex-col md:flex-row">
       <Navbar active="home" />
       <div className="w-full min-h-screen p-4">
         <div className="border h-full">
-          <h1>Hello</h1>
+          {movies.filter((movie) => movie.isTrending).map((movie) => (
+            <div>
+              <li className="text-3xl font-white">{movie.title}</li>
+              <Image src={movie.thumbnail.trending?.large} width={100} height={100} />
+            </div>))}
         </div>
       </div>
     </div>
